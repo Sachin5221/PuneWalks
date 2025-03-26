b5f14c9cfc4a252d852e105c3c907328fb808703
salunkesachin30@gmail.com <salunkesachin30@gmail.com>
2025-03-26 19:19:34 +0530

    UI

---
+++ PunkeWalks.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PunkeWalks.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
